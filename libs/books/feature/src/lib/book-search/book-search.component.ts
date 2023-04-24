18f399b7864fc93401d7585514df98303389ec48
Vigneshwaran SM <vigneshwaran.sm1@T-Mobile.com>
2023-04-24 16:14:36 +0530

    Added instant search

--- libs/books/feature/src/lib/book-search/book-search.component.ts
+++ libs/books/feature/src/lib/book-search/book-search.component.ts
@@ -1,27 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import {
-  addToReadingList,
-  clearSearch,
-  getAllBooks,
-  ReadingListBook,
-  searchBooks
-} from '@tmo/books/data-access';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { Observable } from "rxjs";
+import { debounceTime, distinctUntilChanged, filter, map, switchMap } from "rxjs/operators";
+import { Store } from '@ngrx/store';
+import { addToReadingList, clearSearch, getAllBooks, ReadingListBook, searchBooks } from '@tmo/books/data-access';
 import { Book } from '@tmo/shared/models';
-import {Observable} from "rxjs";
 
 @Component({
   selector: 'tmo-book-search',
   templateUrl: './book-search.component.html',
   styleUrls: ['./book-search.component.scss']
 })
-export class BookSearchComponent {
+export class BookSearchComponent implements OnInit {
   books$: Observable<ReadingListBook[]> = this.store.select(getAllBooks);
 
   searchForm = this.fb.group({
     term: ''
   });
+
+  @ViewChild('bookSearchInput', {static: true}) bookSearchInput: ElementRef;
 
   constructor(
     private readonly store: Store,
@@ -30,6 +27,22 @@ export class BookSearchComponent {
 
   get searchTerm(): string {
     return this.searchForm.value.term;
+  }
+
+  ngOnInit(): void {
+    this.searchForm.get('term').valueChanges.pipe(
+      map((value) => {
+        if (value.trim().length === 0) {
+          this.store.dispatch(clearSearch())
+        }
+        return value;
+      }),
+      filter(value => value.length > 0 && value.trim().length > 0),
+      debounceTime(500),
+      distinctUntilChanged(),
+      switchMap(async () =>
+        this.store.dispatch(searchBooks({term: this.searchTerm})))
+    ).subscribe();
   }
 
   formatDate(date: string): string {
